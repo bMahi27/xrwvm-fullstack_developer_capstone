de8b62f265175bdc8e0016194bc5ee62cbd50924
bMahi27 <bmahitha27@gmail.com>
2026-06-07 09:56:09 -0400

    Added Contact Us page

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -26,10 +26,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-
-    # Home Page
     path('', TemplateView.as_view(template_name="Home.html")),
-
-    # About Page
     path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
