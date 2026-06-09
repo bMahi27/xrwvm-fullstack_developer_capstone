a8e3f30e4bdf6b07df978c3b509f5df9af0a8b95
bMahi27 <bmahitha27@gmail.com>
2026-06-09 03:13:35 -0400

    Added review posting endpoint

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,10 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    'backend_url',
+    default="http://localhost:3030"
+)
+
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="http://localhost:5050/"
+)
 
 
 def get_request(endpoint, **kwargs):
@@ -31,6 +35,7 @@ def get_request(endpoint, **kwargs):
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url + "analyze/" + text
+
     try:
         response = requests.get(request_url)
         return response.json()
@@ -40,4 +45,16 @@ def analyze_review_sentiments(text):
 
 
 def post_review(data_dict):
-    return {}
+    request_url = backend_url + "/insert_review"
+
+    try:
+        response = requests.post(
+            request_url,
+            json=data_dict
+        )
+
+        print(response.json())
+        return response.json()
+
+    except:
+        print("Network exception occurred")
